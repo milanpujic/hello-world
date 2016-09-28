071a3e17937be5d8ff91cef78240bf77cc4ae0d0
milanpujic <milan.pujic@soprex.com>
2016-09-28 10:53:09 +0200

    Update HelloWorldRouteServiceProvider.hh

--- src/Providers/HelloWorldRouteServiceProvider.hh
+++ src/Providers/HelloWorldRouteServiceProvider.hh
@@ -10,6 +10,6 @@
     {
         public function map(Router $router):void
         {
-             $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+             $router->get('hello',hello-world\Controllers\ContentController@sayHello');
         }
     }
